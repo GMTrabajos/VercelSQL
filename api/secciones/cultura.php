9719960d1cbba6c9edd0b51f0a5285f67557d5b3
GMTrabajos <gmtrabajos14@gmail.com>
2025-06-09 12:11:34 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "La Biblioteca Nacional de España digitaliza 30.000 manuscritos históricos",
+  "autor" => "El País Cultura",
+  "resumen" => "Con el objetivo de preservar el patrimonio y facilitar el acceso a investigadores, la BNE ha culminado un ambicioso proyecto de digitalización de obras manuscritas que abarcan desde la Edad Media hasta el siglo XIX. El contenido ya está disponible gratuitamente en su plataforma digital Hispana."
 ];
 ?>
