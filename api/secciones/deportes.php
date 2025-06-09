7ba7bd200e294683ebef446470c2f594b4b3e39f
GMTrabajos <gmtrabajos14@gmail.com>
2025-06-09 12:15:40 -0500

    Update deportes.php

--- api/secciones/deportes.php
+++ api/secciones/deportes.php
@@ -7,5 +7,7 @@ $deportes = [
   "titulo" => "Kylian Mbappé debuta con gol en el Real Madrid y enciende el Bernabéu",
   "autor" => "Agencia Reuters Deportes",
   "resumen" => "El delantero francés Kylian Mbappé marcó en su primer partido oficial con el Real Madrid, generando entusiasmo entre la afición. El gol llegó en el minuto 38 tras una asistencia de Vinícius Jr., en un encuentro que terminó 3-1 frente al Sevilla. Con esto, el fichaje más esperado del año arranca con gran expectativa."
+  "imagen" => "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Kylian_Mbapp%C3%A9_2019.jpg/640px-Kylian_Mbapp%C3%A9_2019.jpg"
+];
 ];
 ?>
