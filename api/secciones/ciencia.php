5a6eecf1792d3688444e38f27b7f956db5f37ba0
GMTrabajos <gmtrabajos14@gmail.com>
2025-06-09 12:11:24 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "La NASA detecta una señal de radio repetitiva desde una galaxia lejana",
+  "autor" => "Redacción Ciencia – Agencia EFE",
+  "resumen" => "Astrónomos de la NASA informaron sobre la detección de ráfagas rápidas de radio (FRB) que se repiten con un patrón regular desde una galaxia a más de 1.000 millones de años luz. Este fenómeno, todavía no explicado por completo, podría ayudar a entender mejor la estructura del universo y los objetos extremos como los magnetares."
 ];
 ?>
